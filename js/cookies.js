b7195954d6e2612f8ec7f74237def6f3923c8b2e
lausek <root@lausek.eu>
2018-05-22 19:19:28 +0200

    warning without js!

--- js/cookies.js
+++ js/cookies.js
@@ -1,28 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     "use strict";
-    
+
     var cookie = decodeURIComponent(document.cookie).split(";");
     for (var i = 0; i < cookie.length; i++) {
         var parts = cookie[i].split("=");
         if (parts[0] === "cookies-allowed") {
+            document.getElementById("box-cookie-warning").remove();
             return; 
         }
     }
 
-    var msg = document.createElement("div");
-    msg.innerHTML = "This site uses cookies. <a href='https://wikipedia.org/wiki/Magic_cookie'>Learn more...</a>"; 
-    msg.setAttribute("style", "height:auto;padding:10px;margin-top:10px;;vertical-align:middle;background-color:red;");
-
-    var conf = document.createElement("button");
-    conf.innerHTML = "okay";
-    conf.setAttribute("style", "float:right;");
-    conf.onclick = function () {
+    document.getElementById("box-confirm-cookie").onclick = function () {
         document.cookie += "cookies-allowed=1"; 
-        document.body.removeChild(msg);
+        document.getElementById("box-cookie-warning").remove();
     };
-
-    msg.appendChild(conf);
-    document.body.prepend(msg);
 
 });
