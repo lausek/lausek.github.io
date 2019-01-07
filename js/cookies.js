c35e566f597c780d19f6913d817ef8b064d6f950
lausek <root@lausek.eu>
2019-01-07 19:13:53 +0100

    added cookie expire for 30 days

--- js/cookies.js
+++ js/cookies.js
@@ -12,7 +12,10 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     document.getElementById("box-confirm-cookie").onclick = function () {
-        document.cookie += "cookies-allowed=1"; 
+        var date = new Date();
+        // set expire after 30 days
+        date.setTime(date.getTime() + (30 * 24 * 60 * 60 * 1000));
+        document.cookie += "cookies-allowed=1;expires=" + date.toUTCString() + ";path=/";
         document.getElementById("box-cookie-warning").remove();
     };
 
